24a3e74fb9bb055e1beac1d7f06870b99fb5902f
Maciej Waleryn <xvexisu@gmail.com>
2022-11-08 15:36:09 +0100

    Add plain project files.

---
+++ src/main/java/pl/polsl/student/barberio/BarberioApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.student.barberio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/polsl/student/barberio/BarberioApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.polsl.student.barberio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
