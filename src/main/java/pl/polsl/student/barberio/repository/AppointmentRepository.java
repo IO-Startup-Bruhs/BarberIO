41878e436e665572dceef2ec253d9a4a02d1f138
DirtySand <72354911+DirtySand@users.noreply.github.com>
2023-01-08 16:01:28 +0100

    Added customer appointments

--- src/main/java/pl/polsl/student/barberio/repository/AppointmentRepository.java
+++ src/main/java/pl/polsl/student/barberio/repository/AppointmentRepository.java
@@ -3,5 +3,8 @@ package pl.polsl.student.barberio.repository;
 import org.springframework.data.repository.CrudRepository;
 import pl.polsl.student.barberio.model.Appointment;
 
+import java.util.List;
+
 public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
+    List<Appointment> findByCustomerPhoneNumber(String phoneNumber);
 }
