b240643600f9d07019e37f9cae01655fa7ee4582
VoXelGDesign <trynewgame@gmail.com>
2023-01-28 08:46:44 +0100

    Added dark theme to client/newappointment and some other small visual improvements

--- src/main/java/pl/polsl/student/barberio/controller/client/ClientAppointmentsController.java
+++ src/main/java/pl/polsl/student/barberio/controller/client/ClientAppointmentsController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import pl.polsl.student.barberio.impl.DatabaseUserDetails;
 import pl.polsl.student.barberio.model.User;
 import pl.polsl.student.barberio.service.AppointmentService;
