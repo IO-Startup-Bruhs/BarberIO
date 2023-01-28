3d5a29ce7cf56bd6a421c94124e0ec4c4fa18bee
VoXelGDesign <trynewgame@gmail.com>
2023-01-28 16:27:24 +0100

    Confirming and canceling is now secured, small refactoring in ClientAppointmentsController

--- src/main/java/pl/polsl/student/barberio/controller/client/ClientAppointmentsController.java
+++ src/main/java/pl/polsl/student/barberio/controller/client/ClientAppointmentsController.java
@@ -1,13 +1,12 @@
 package pl.polsl.student.barberio.controller.client;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pl.polsl.student.barberio.impl.DatabaseUserDetails;
-import pl.polsl.student.barberio.model.User;
 import pl.polsl.student.barberio.service.AppointmentService;
 
 @Controller
@@ -15,12 +14,15 @@ public class ClientAppointmentsController {
     private AppointmentService appointmentService;
 
     @GetMapping("/client/appointments")
-    public String showTableWithAppointments(Model model, User user) {
-        DatabaseUserDetails databaseUserDetails = (DatabaseUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        user= databaseUserDetails.getUser();
-        // ^to pewnie można zrobić lepiej, ale też działa ... przynajmniej narazie
-        model.addAttribute("appointments",this.appointmentService.getAppointments(user));
+    public String showTableWithAppointments(Model model, @AuthenticationPrincipal DatabaseUserDetails principal) {
+        model.addAttribute("appointments",this.appointmentService.getAppointments(principal.getUser()));
         return "client/appointments";
+    }
+
+    @GetMapping("/cancelAppointment/client/{id}")
+    public String cancelAppointment(@PathVariable long id, @AuthenticationPrincipal DatabaseUserDetails principal){
+        appointmentService.setCancelled(id,principal);
+        return "redirect:/client/appointments";
     }
 
     @Autowired
--- src/main/java/pl/polsl/student/barberio/service/AppointmentService.java
+++ src/main/java/pl/polsl/student/barberio/service/AppointmentService.java
@@ -1,8 +1,11 @@
 package pl.polsl.student.barberio.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 import pl.polsl.student.barberio.form.NewAppointmentForm;
+import pl.polsl.student.barberio.impl.DatabaseUserDetails;
 import pl.polsl.student.barberio.model.Appointment;
 import pl.polsl.student.barberio.model.User;
 import pl.polsl.student.barberio.repository.AppointmentRepository;
@@ -17,20 +20,31 @@ public class AppointmentService {
 
     private AppointmentRepository appointmentRepository;
 
-    public void setCancelled(long appointmentId) {
+    public void setCancelled(long appointmentId, DatabaseUserDetails principal) {
         Optional<Appointment> appointment = appointmentRepository.findById(appointmentId);
         appointment.ifPresent(a -> {
-            a.setCancelled(true);
-            appointmentRepository.save(a);
+
+            if((principal.getUser().getId() == a.getCustomer().getId())
+                    || (principal.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN")))
+                    || (principal.getAuthorities().contains(new SimpleGrantedAuthority("EMPLOYEE"))))
+            {
+                a.setCancelled(true);
+                appointmentRepository.save(a);
+            }
+
         });
 
     }
 
-    public void setConfirmed(long appointmentId) {
+    public void setConfirmed(long appointmentId, DatabaseUserDetails principal) {
         Optional<Appointment> appointment = appointmentRepository.findById(appointmentId);
         appointment.ifPresent(a -> {
-            a.setConfirmation(true);
-            appointmentRepository.save(a);
+            if(principal.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"))
+                    || principal.getAuthorities().contains(new SimpleGrantedAuthority("EMPLOYEE")))
+            {
+                a.setConfirmation(true);
+                appointmentRepository.save(a);
+            }
         });
 
     }
